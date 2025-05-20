b2236e4234d01281df50fc9c4857db0a588b3cf5
Huynh Duy Dong <149409582+huynhduydong@users.noreply.github.com>
2025-05-20 15:51:30 +0700

    Use env vars for credentials

--- LavenShopBackend/src/main/java/com/dong/contants/PaypalConstants.java
+++ LavenShopBackend/src/main/java/com/dong/contants/PaypalConstants.java
@@ -1,8 +1,26 @@
 package com.dong.contants;
 
-public interface PaypalConstants {
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-    String BASE_URI = "https://api-m.sandbox.paypal.com";
-    String CLIENT_ID  = "ASViZj3VChGkol3QG1RfeBT1R11y1wies-IXaBtVSdKWu9vc5mU1tzBxWQuSkkrsvX9gRb4FI9ZXWTR3";
-    String CLENT_SECRET = "EJsVisLY654yUhiqBJdxsK-p92HMA6Q-lXhRqEk2sAVugXDSBSO8S7KtVjjS3-V47vfsk-8PvJynbhKp";
+@Component
+public class PaypalConstants {
+
+    public static final String BASE_URI = "https://api-m.sandbox.paypal.com";
+
+    public static String CLIENT_ID;
+    public static String CLENT_SECRET;
+
+    @Value("${PAYPAL_CLIENT_ID}")
+    private String clientId;
+
+    @Value("${PAYPAL_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @PostConstruct
+    public void init() {
+        CLIENT_ID = clientId;
+        CLENT_SECRET = clientSecret;
+    }
 }
